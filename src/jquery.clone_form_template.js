242b6acd0b3d0bcbb9d703732e03e4b523600761
Samuel Cole <sam@samuelcole.name>
2014-10-06 12:26:41 -0400

    clone form template: you have to opt in to copying form values

--- src/jquery.clone_form_template.js
+++ src/jquery.clone_form_template.js
@@ -1,22 +1,22 @@
 (function ($) {
-  $.fn.clone_form_template = function () {
+  $.fn.clone_form_template = function (options) {
     return $(this).map(function () {
-      return update($(this).clone())[0];
+      return update($(this).clone(), options)[0];
     });
   };
 
   // updates a template clone according to conventions
-  function update($clone) {
+  function update($clone, options) {
     var index = new Date().getTime();
 
     $clone.find('label,input,textarea,select').each(function (i, obj) {
-      update_attrs($(obj), index);
+      update_attrs($(obj), index, options);
     });
 
     return $clone.removeClass('template').show();
   }
 
-  function update_attrs($element, identifier) {
+  function update_attrs($element, identifier, options) {
     if ($element.attr('id')) {
       $element.attr('id',
         $element.attr('id').replace(/_-?\d+_/, '_' + identifier + '_')
@@ -28,7 +28,9 @@
       );
     }
     if ($element.attr('name')) {
-      $element.val($('[name="' + $element.attr('name') + '"]').val());
+      if (options.copy_values) {
+        $element.val($('[name="' + $element.attr('name') + '"]').val());
+      }
       $element.attr('name',
         $element.attr('name').replace(/\[-?\d+\]/, '[' + identifier + ']')
       );
